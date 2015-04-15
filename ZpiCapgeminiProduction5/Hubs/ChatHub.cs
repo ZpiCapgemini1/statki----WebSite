ba41724f0afa50da743aefe6c51a682e1adf5670
przemo098 <przemo098@hotmail.com>
2015-04-15 19:19:13 +0200

    Strona v1.0
    chat / gra

---
+++ ZpiCapgeminiProduction5/Hubs/ChatHub.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ZpiCapgeminiProduction5.Hubs
+{
+    public static class UserHandler
+    {
+        public static HashSet<string> ConnectedIds = new HashSet<string>();
+    }
+
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // for all users
+            Clients.All.addNewMessageToPage(name, message);
+        }
+
+
+        public override Task OnConnected()
+        {
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
